8fca2d51e5c44c9bc4cb59e883b6dd16541163ff
mehdiRma10 <mehdi.boucenna06@gmail.com>
2017-09-28 16:40:30 -0400

    added.

--- lib/Mehdirma10/CSS/Renderable.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mehdirma10\CSS;
-
-interface Renderable {
-	public function __toString();
-	public function render(\Mehdirma10\CSS\OutputFormat $oOutputFormat);
-	public function getLineNo();
-}
--- lib/Mehdirma10/CSS/Value/RuleValueList.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mehdirma10\CSS\Value;
-
-class RuleValueList extends ValueList {
-	public function __construct($sSeparator = ',', $iLineNo = 0) {
-		parent::__construct(array(), $sSeparator, $iLineNo);
-	}
-}
